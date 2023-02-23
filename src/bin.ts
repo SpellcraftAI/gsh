c265c700c346ec7442108d89e73445474016e5b1
Marina Cabrera <contact@marinacabrera.dev>
2023-02-23 10:08:15 -0300

    feat: Updated files according to latest changes, executing commands by default

--- src/bin.ts
+++ src/bin.ts
@@ -1,58 +1,52 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-import { clear, error, log, style } from "@tsmodule/log";
-import { AUTH0_CLIENT } from "./globs/node";
 import { createInterface } from "readline";
 import { stdin, stdout } from "process";
-import { VERSION } from "./globs/shared";
 import { appendToTranscript, getTranscript } from "./utils/filesystem";
-import { fetchResponseFromApi } from "./utils/api";
-import { readFile } from "fs/promises";
+import { fetchResponseFromApi, checkAuth } from "./utils/api";
+import {
+  displayDimmed,
+  displayLogoAndVersion,
+  displayWarning,
+  executeCommand,
+  promptStyle,
+  trimLinePrefixes,
+} from "./utils/console";
 
-const authorized = await AUTH0_CLIENT.isAuthorized();
-if (!authorized) {
-  error("You are not logged in.", [], { postLines: 1 });
-
-  log("Use UPG to log in using `upg login`:", ["dim"]);
-  log("https://gptlabs.us/upg", ["underline"]);
-
-  process.exit(1);
-}
+checkAuth();
 
 const rl = createInterface({
   input: stdin,
-  output: stdout,
+  output: stdout
 });
 
-rl.on("SIGINT", () => {
-  process.exit(1);
-});
-
-clear({ flush: true });
-
-const logoPath = new URL("./header.txt", import.meta.url);
-const logoFile = await readFile(logoPath, "utf8");
-const logoText = logoFile.replace("(A version number goes here)", VERSION);
-
-console.log(style(logoText, ["dim"]));
+const isDryMode = process.argv.slice(2).includes('--dry-mode');
+if (isDryMode) {
+  displayWarning("You're running in dry-mode, no command will be executed.");
+}
+await displayLogoAndVersion();
 
 while (true) {
   const transcript = await getTranscript();
   const command = await new Promise<string>((resolve) => {
-    rl.question(` ${style("$", ["bold", "dim"])} `, resolve);
+    rl.question(promptStyle, resolve);
   });
-  // console.log({ transcript });
-  console.log();
-  // console.log(style(command, ["dim"]));
 
-  const { native } = await fetchResponseFromApi(command, transcript)
-
-  const replacedLinePrefixes = native.trim().split("\n").map(
-    (line: string) => line.replace(/^\$ /, "")
-  ).join("\n");
-
-  console.log(style(replacedLinePrefixes, ["dim"]));
-  console.log();
-
-  await appendToTranscript(command, native);
+  try {
+    const { native } = await fetchResponseFromApi(command, transcript)
+    const replacedLinePrefixes = trimLinePrefixes(native)
+    displayDimmed(replacedLinePrefixes);
+    console.log();
+    if (!isDryMode) {
+      await executeCommand(replacedLinePrefixes);
+      console.log();
+    } else {
+      displayDimmed(replacedLinePrefixes);
+      console.log();
+    }
+    await appendToTranscript(command, native);
+  } catch (e) {
+    console.log();
+    displayWarning("There was an error fetching the response from the API.");
+  }
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,24 @@
 import { AUTH0_CLIENT } from "../globs/node";
+import { error, log } from "@tsmodule/log";
 import { DOMAIN_URL, PLATFORM } from "../globs/shared";
 
+export async function checkAuth() {
+  const authorized = await AUTH0_CLIENT.isAuthorized();
+  if (!authorized) {
+    error("You are not logged in.", [], { postLines: 1 });
+
+    log("Use UPG to log in using `upg login`:", ["dim"]);
+    log("https://gptlabs.us/upg", ["underline"]);
+
+    process.exit(1);
+  }
+}
+interface shellResponse {
+  native: string;
+}
+
 export const fetchResponseFromApi = async (command: string, transcript: string) => {
-    return await AUTH0_CLIENT.fetch(
+  return await AUTH0_CLIENT.fetch(
     `${DOMAIN_URL}/api/gsh/shell`,
     {
       method: "POST",
@@ -12,5 +28,5 @@ export const fetchResponseFromApi = async (command: string, transcript: string) 
         transcript,
       })
     }
-  ).then((res) => res.json()) as any;
+  ).then((res) => res.json()) as shellResponse;
 }
