d18b2d23fdeb4a9aea9132bf01c7e6faccb67a4a
Marina Rosa <contato@marinacabrera.dev>
2023-05-24 09:01:34 -0300

    chore: update auth0 url (#14)

--- src/globs/node.ts
+++ src/globs/node.ts
@@ -3,7 +3,7 @@ import { homedir } from "os";
 import { resolve } from "path";
 
 export const AUTH0_CLIENT = new Auth0NodeClient({
-  domain: "gptlabs.us.auth0.com",
+  domain: "auth.spellcraftai.com",
   clientId: "I3rJAbl7D09DuYG6dGeYWtBtpuGyeZrI",
   audience: "https://gptlabs.us.auth0.com/api/v2/",
 });
